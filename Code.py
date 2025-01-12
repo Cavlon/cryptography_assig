7fdf570e8a50b3cb4ea02dc4bb5e153a692f99ae
Cavlon <zmzf47@durham.ac.uk>
2025-01-12 19:47:43 +0000

    Final Build

--- Code.py
+++ Code.py
@@ -1,25 +1,26 @@
 import numpy as np
 import math
+import random
 import time
 
-def repeated_squaring(generator, target_exponent, modulo):
-    power = target_exponent
-    current_exponentiation = generator
-    result = 1
+# def modular_pow(a, b, p):
+#     power = b
+#     current_exponentiation = a
+#     result = 1
 
-    # The binary of the exponent shows which powers of 2 should be included in the result
-    binary = bin(target_exponent)
+#     # The binary of the exponent shows which powers of 2 should be included in the result
+#     binary = bin(a)
 
-    for i in range(len(binary)-2):
-        # Only include the required powers
-        if power % 2 == 1:
-            result = (result * current_exponentiation) % modulo
+#     for i in range(len(binary)-2):
+#         # Only include the required powers
+#         if power % 2 == 1:
+#             result = (result * current_exponentiation) % p
         
-        # Repeatedly square
-        current_exponentiation = (current_exponentiation * current_exponentiation) % modulo
-        power = power >> 1
+#         # Repeatedly square
+#         current_exponentiation = (current_exponentiation * current_exponentiation) % p
+#         power = power >> 1
     
-    return result
+#     return result
 
 # Identical to the standard recursive euclidean algorithm but includes the extra equations
 def extended_euclidean(a, b, s1 = 1, s2 = 0, t1 = 0, t2 = 1):
@@ -29,9 +30,10 @@ def extended_euclidean(a, b, s1 = 1, s2 = 0, t1 = 0, t2 = 1):
         q = a // b
         return extended_euclidean(b, a % b, s2, s1-(q*s2), t2, t1-(q*t2))
 
-# Miller-Rabin Primality test from Wikipedia
+# Miller-Rabin Primality test following Wikipedia pseudocode
 def primality_test(n, k):
 
+    # These cases are hord-coded or else it breaks
     if n < 4:
         return n == 2 or n == 3
     elif n % 2 == 0:
@@ -44,8 +46,9 @@ def primality_test(n, k):
         s += 1
     
     for i in range(k):
-        a = np.random.randint(2, n-1)
-        x = repeated_squaring(a, d, n)
+        # a = random.randrange(2, n-1)
+        a = int(np.random.random() * (n-3)) + 2
+        x = pow(a, d, n)
         if x == 1 or x == n - 1:
             continue
 
@@ -63,9 +66,10 @@ def primality_test(n, k):
             return False
     return True
 
-def rho_function(val):
-    return val * val + 1
+def rho_function(val, mod):
+    return (pow(val, 2, mod) + 1) % mod
 
+# Algorithm follows the paper's pseudocode
 def brent_pollard_rho_factorise(n, x0, m):
     x = x0
     y = x0
@@ -80,14 +84,14 @@ def brent_pollard_rho_factorise(n, x0, m):
         x = y
 
         for i in range(r):
-            y = rho_function(y) % n
+            y = rho_function(y, n)
         
         k = 0
         while k < r and G == 1:
             ys = y
             count = min(m, r-k)
             for i in range(count):
-                y = rho_function(y) % n
+                y = rho_function(y, n)
                 q = (q * abs(x-y)) % n
             G = extended_euclidean(q, n)[0]
             k += m
@@ -95,10 +99,10 @@ def brent_pollard_rho_factorise(n, x0, m):
         r *= 2
     
     if G == n:
-        ys = rho_function(ys) % n
+        ys = rho_function(ys, n)
         G = extended_euclidean(abs(x - ys), n)[0]
         while G == 1:
-            ys = rho_function(ys) % n
+            ys = rho_function(ys, n)
             G = extended_euclidean(abs(x - ys), n)[0]
     
     if G == n:
@@ -107,7 +111,7 @@ def brent_pollard_rho_factorise(n, x0, m):
         return G
 
 def prime_factorise(val, m):
-    small_primes = [2, 3, 5, 7, 11, 13, 17]
+    small_primes = [2, 3, 5, 7, 11, 13, 17, 19]
     factors = []
     n = val
 
@@ -128,65 +132,77 @@ def prime_factorise(val, m):
         prime_factor = n
 
         # Repeatedly find factors of factors until a prime factor is found
-        while primality_test(prime_factor, 2) == False:
+        while primality_test(prime_factor, 4) == False:
             factor = prime_factor
-            prime_factor = brent_pollard_rho_factorise(factor, np.random.randint(1, factor), m)
+            prime_factor = brent_pollard_rho_factorise(factor, int(np.random.random() * (factor-1)) + 1, m)
 
             # Repeat with a different x0 if Pollard Rho fails
             while prime_factor == False:
-                prime_factor = brent_pollard_rho_factorise(factor, np.random.randint(1, factor), m)
+                prime_factor = brent_pollard_rho_factorise(factor, int(np.random.random() * (factor-1)) + 1, m)
 
         # Calculate the power factor
         power = 0
         while n % prime_factor == 0:
             n = n // prime_factor
             power += 1
-        
+
         # Amend the list of power factors
         factors.append((prime_factor, power))
     
     return factors
 
 def shank_discrete_log(p, g, A, cardinality):
+    print(A, cardinality)
+
+    start = time.time()
 
     target = A
 
-    # Cardinality is used since the sub-group has lower cardinality but the order is the same
-    m = math.ceil(math.sqrt(cardinality))
+    # Cardinality is used since the sub-group has lower cardinality but the modulus is the same
+    m = math.sqrt(cardinality)
+    m_large = math.ceil(m * 2)
+    m_small = math.ceil(m / 2)
 
-    # Dictionary holding {value:power of the generator to attain this value}
-    big_steps = {1:0}
+    # Set for fast existence check, array for index search
+    big_steps_set = set()
+    big_steps_set.add(1)
+    big_steps = [1]
 
     val = 1
+
     # A big step in the sub-group
-    step = repeated_squaring(g, m, p)
+    step = pow(g, m_large, p)
 
     # Compute all the big steps
-    for i in range(1, m):
+    for i in range(1, m_small):
         val = (val * step) % p
-        big_steps[val] = i*m
+        big_steps.append(val)
+        big_steps_set.add(val)
+
+    print(time.time() - start)
     
-    # Compute all the small steps
-    for i in range(m):
-        if target in big_steps:
-            return (big_steps[target] - i) % (p-1)
+    # Iterate through the small steps until a collision
+    for i in range(m_large):
+        if target in big_steps_set:
+            print(time.time() - start)
+
+            return ((big_steps.index(target) * m_large) - i) % (p-1)
         target = (target * g) % p
 
 def DiffieHellman(p, g, B):
-    a = np.random.randint(2, p-2)
-    print(a)
-    A = repeated_squaring(g, a, p)
-    K = repeated_squaring(B, a, p)
+    a = np.random.randint(3, p-2)
+    A = pow(g, a, p)
+    K = pow(B, a, p)
     return A, K
 
 def ElGamalEncrypt(p, g, A, x):
-    k = np.random.randint(1, p-1)
-    y1 = repeated_squaring(g, k, p)
-    y2 = (x * repeated_squaring(A, k, p)) % p
+    k = np.random.randint(0, p-1)
+    y1 = pow(g, k, p)
+    y2 = (x * pow(A, k, p)) % p
     return y1, y2
 
 def ElGamalDecrypt(p, a, y1, y2):
-    power = repeated_squaring(y1, a, p)
+    power = pow(y1, a, p)
     inverse = extended_euclidean(power, p)[1] % p
     return (y2 * inverse) % p
 
@@ -195,22 +211,23 @@ def DiscreteLog(p, g, A):
     # Use Silver-Pholig-Hellman to split it into several smaller discrete log instances
     factors = prime_factorise(p-1, 800)
 
-    # print(factors)
+    print(factors)
 
     # The final result of the discrete log
     res = 0
 
+    # generator_inverse = extended_euclidean(g, p)[1] % p
+    generator_inverse = pow(g, p-2, p)
+    p_min_1 = p-1
+
     for factor, order in factors:
 
         # These change according to the order of the iteration
-        power = (p-1)
+        power = p_min_1
         factor_power = 1
 
         # The generators are constant so they are pre-computed
-        generator = repeated_squaring(g, power // factor, p)
-        generator_inverse = extended_euclidean(g, p)[1] % p
-
-        # print(f"gener = {generator}, inve = {generator_inverse}")
+        generator = pow(g, power // factor, p)
 
         # The value being logged at each iteration
         beta = A
@@ -224,7 +241,7 @@ def DiscreteLog(p, g, A):
             power = power // factor
 
             # Initialise the beta
-            instance_beta = repeated_squaring(beta, power, p)
+            instance_beta = pow(beta, power, p)
 
             # If the size is 2 then the sqrt rounds to 2 which messes up the modulo calculations
             if factor == 2:
@@ -233,7 +250,6 @@ def DiscreteLog(p, g, A):
                 else:
                     coefficient = 1
             else:
-
                 # Solve the smaller discrete log instance
                 coefficient = shank_discrete_log(p, generator, instance_beta, factor)
 
@@ -242,100 +258,61 @@ def DiscreteLog(p, g, A):
             # Update variables
             factor_res += coefficient_result
             factor_power *= factor
-            beta *= repeated_squaring(generator_inverse, coefficient_result, p)
+            beta *= pow(generator_inverse, coefficient_result, p)
 
         # Combine the results by the Chinese remainder theorem
         inverse = extended_euclidean(power, factor_power)[1] % factor_power
-        res = (res + (power * inverse * factor_res)) % (p-1)
+        res = (res + (power * inverse * factor_res)) % p_min_1
 
     return res
 
 def DiffieHellmanCrack(p, g, A, B):
     a = DiscreteLog(p, g, A)
-    return repeated_squaring(B, a, p)
+    return pow(B,a,p)
 
 def ElGamalCrack(p, g, A, y1, y2):
     a = DiscreteLog(p, g, A)
-    power = repeated_squaring(y1, a, p)
+    power = pow(y1,a,p)
     inverse = extended_euclidean(power, p)[1] % p
     return (y2 * inverse) % p
 
+g = 33026900258717130131626338278
+a = int(np.random.random() * 100003100019100043100057100069)
+p = 100003100019100043100057100069
 
-def find_generator(p):
-    factors = prime_factorise(p-1, 800)
-    found = True
+# # g = 3
+# # a = 5
+# # p = 7
 
-    for i in range(1, p):
-
-        for factor, order in factors:
-            power = (p-1) // factor
-            if repeated_squaring(i, power, p) == 1:
-                found = False
-                break
-        
-        if found:
-            return i
-        else:
-            found = True
-    return False
-
-# g = 7
-# a = 178162
-# p = 999959
-# A = repeated_squaring(g,a,p)
-# print(f"A = {A}")
-
-# print(find_generator(p))
-
-# print(shank_discrete_log(p,g,A,p))
-# print(pollard_rho_discrete_log(p, g, A, p//3))
-# print(DiscreteLog(p, g, A))
-
-# print(extended_euclidean(6,16))
-
-## PROFILING ##
-
-trials = 10000
-tot = 0
-
-for i in range(trials):
-    # print(i)
-    # a = np.random.randint(1,p)
-    # A = repeated_squaring(g,a,p)
-    value = np.random.randint(100, 100000000)
-    start = time.time()
-    # res = shank_discrete_log(p, g, A, p)
-    # brent_pollard_rho_discrete_log(p,g,A,p//3)
-    # pollard_rho_discrete_log(p,g,A,p//3)
-    brent_pollard_rho_factorise(value, np.random.randint(1, value), 800)
-    # res = DiscreteLog(p,g,A)
-    end = time.time()
-    # if a != res:
-    #     print("incorrect")
-    tot += end - start
-print(tot/trials)
-# print(pollard_rho_discrete_log(p,g,A,p//3))
-# print(pollard_rho_discrete_log(p,49,9,p//3, (p-1)//2))
-# print(pollard_rho_discrete_log(7,2,4,p//3, (7-1)//2))
-# print(shank_discrete_log(p, 49, 9, (p-1)//2))
-# print(DiscreteLog(p, g, A))
+A = pow(g,a,p)
+print(f"A = {A}")
+start = time.time()
+print(DiscreteLog(p, g, A))
+print(time.time() - start)
+# print(recursed_discrete_log(p, g, A, p))
+# # print(pow(g,res,p))
 
 
-# trials = 100
-# min_m = 1
-# min_time = 100000
+# # if (pow(g,res,p) != A):
+# #     print("wrong")
 
-# for m in range(500,1001):
-#     tot = 0
+# ## PROFILING ##
 
-#     for i in range(trials):
-#         start = time.time()
-#         brent_prime_factorise(np.random.randint(100, 100000000), m)
-#         # prime_factorise(104728)
-#         end = time.time()
-#         tot += end - start
-#     if tot/trials < min_time:
-#         min_time = tot/trials
-#         min_m = m
-# print(min_time)
-# print(min_m)
+# trials = 1000
+# tot = 0
+
+# for i in range(trials):
+#     # a = np.random.randint(0, p-1)
+#     # A = pow(g,a,p)
+
+#     start = time.time()
+#     res = DiscreteLog(p, g, A)
+#     # res = shank_discrete_log(p, g, A, p)
+#     end = time.time()
+#     tot += end - start
+#     if res[0] != a:
+#         print("wrong")
+#         print(res, a)
+# print(tot/trials)
+
+# # print(DiscreteLog(p, g, A))
